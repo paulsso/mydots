4b8af3f76c9dc8f43ba9fd5488a665ce41f9fed7
Oskar Paulsson <oskar@paulsson.dev>
2025-06-23 21:08:49 +0200

    update

--- scripts/settings/nvim/lua/plugins.lua
+++ scripts/settings/nvim/lua/plugins.lua
@@ -40,13 +40,50 @@ return {
  {
   "lewis6991/gitsigns.nvim",
   config = function()
-    require("gitsigns").setup()
+    require("gitsigns").setup {
+      on_attach = function(bufnr)
+        local gs = package.loaded.gitsigns
+
+        local function map(mode, l, r, opts)
+          opts = opts or {}
+          opts.buffer = bufnr
+          vim.keymap.set(mode, l, r, opts)
+        end
+
+        -- Navigation
+        map("n", "]c", function()
+          if vim.wo.diff then return "]c" end
+          vim.schedule(function() gs.next_hunk() end)
+          return "<Ignore>"
+        end, { expr = true, desc = "Next Hunk" })
+
+        map("n", "[c", function()
+          if vim.wo.diff then return "[c" end
+          vim.schedule(function() gs.prev_hunk() end)
+          return "<Ignore>"
+        end, { expr = true, desc = "Prev Hunk" })
+
+        -- Actions
+        map("n", "<leader>hs", gs.stage_hunk, { desc = "Stage Hunk" })
+        map("n", "<leader>hr", gs.reset_hunk, { desc = "Reset Hunk" })
+        map("v", "<leader>hs", function() gs.stage_hunk { vim.fn.line("."), vim.fn.line("v") } end, { desc = "Stage Hunk" })
+        map("v", "<leader>hr", function() gs.reset_hunk { vim.fn.line("."), vim.fn.line("v") } end, { desc = "Reset Hunk" })
+
+        map("n", "<leader>hp", gs.preview_hunk, { desc = "Preview Hunk" })
+        map("n", "<leader>hb", gs.blame_line, { desc = "Blame Line" })
+
+        map("n", "<leader>tb", gs.toggle_current_line_blame, { desc = "Toggle Line Blame" })
+      end,
+    }
   end,
   },
   {
   "mfussenegger/nvim-dap",
   dependencies = {
     "rcarriga/nvim-dap-ui",
+	dependencies = {
+		"nvim-neotest/nvim-nio",
+	}
   },
   config = function()
     local dap = require("dap")
