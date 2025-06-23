0cbaf030ce2d33465011b1fba0b6dac9bebd51c7
Oskar Paulsson <oskar@paulsson.dev>
2025-06-23 13:11:12 +0200

    update

--- scripts/settings/nvim/lua/plugins.lua
+++ scripts/settings/nvim/lua/plugins.lua
@@ -36,6 +36,34 @@ return {
       require("spectre").toggle()
     end, { desc = "Toggle Spectre (Search & Replace)", noremap = true, silent = true })
   end,
- }
-}
+ },
+ {
+  "lewis6991/gitsigns.nvim",
+  config = function()
+    require("gitsigns").setup()
+  end,
+  },
+  {
+  "mfussenegger/nvim-dap",
+  dependencies = {
+    "rcarriga/nvim-dap-ui",
+  },
+  config = function()
+    local dap = require("dap")
+    local dapui = require("dapui")
 
+    dapui.setup()
+    -- Automatically open dapui when starting debugger
+    dap.listeners.after.event_initialized["dapui_config"] = function()
+      dapui.open()
+    end
+    -- Automatically close dapui when ending debugger
+    dap.listeners.before.event_terminated["dapui_config"] = function()
+      dapui.close()
+    end
+    dap.listeners.before.event_exited["dapui_config"] = function()
+      dapui.close()
+    end
+  end,
+  }
+}
