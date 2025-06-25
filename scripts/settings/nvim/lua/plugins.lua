039efc9394b396aa0b580dc349163c321b387983
codebro <64486383+paulsso@users.noreply.github.com>
2025-06-25 08:45:52 +0200

    Update plugins.lua

--- scripts/settings/nvim/lua/plugins.lua
+++ scripts/settings/nvim/lua/plugins.lua
@@ -1,106 +1,150 @@
--- ~/.config/nvim/lua/plugins.lua
-
 return {
+  -- Nord theme
   {
     "gbprod/nord.nvim",
-    lazy = false,  -- Load immediately
+    lazy = false,
     priority = 1000,
     config = function()
       vim.cmd("colorscheme nord")
     end,
   },
+
+  -- NvimTree (file browser)
   {
-	  "nvim-telescope/telescope.nvim",
-	  dependencies = { "nvim-lua/plenary.nvim" },
+    "nvim-tree/nvim-tree.lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("nvim-tree").setup {}
+    end,
   },
+
+  -- Telescope
   {
-  "nvim-telescope/telescope-live-grep-args.nvim",
-  dependencies = { "nvim-telescope/telescope.nvim" },
-  config = function()
-    require("telescope").load_extension("live_grep_args")
-  end,
+    "nvim-telescope/telescope.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
   },
+
+  -- Spectre
+  { "nvim-pack/nvim-spectre" },
+
+  -- Git signs
   {
-  "nvim-tree/nvim-tree.lua",
-  dependencies = { "nvim-tree/nvim-web-devicons" }, -- optional, for file icons
-  config = function()
-    require("nvim-tree").setup {}
-    -- Keybinding to toggle file browser
-    vim.keymap.set("n", "<C-n>", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
-  end,
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require("gitsigns").setup {
+        on_attach = function(bufnr)
+          local gs = package.loaded.gitsigns
+          local function map(mode, l, r, opts)
+            opts = opts or {}
+            opts.buffer = bufnr
+            vim.keymap.set(mode, l, r, opts)
+          end
+          map("n", "]c", function() if vim.wo.diff then return "]c" end vim.schedule(gs.next_hunk) return "<Ignore>" end, { expr = true })
+          map("n", "[c", function() if vim.wo.diff then return "[c" end vim.schedule(gs.prev_hunk) return "<Ignore>" end, { expr = true })
+          map("n", "<leader>hs", gs.stage_hunk)
+          map("n", "<leader>hr", gs.reset_hunk)
+          map("v", "<leader>hs", function() gs.stage_hunk { vim.fn.line("."), vim.fn.line("v") } end)
+          map("v", "<leader>hr", function() gs.reset_hunk { vim.fn.line("."), vim.fn.line("v") } end)
+          map("n", "<leader>hp", gs.preview_hunk)
+          map("n", "<leader>hb", gs.blame_line)
+          map("n", "<leader>tb", gs.toggle_current_line_blame)
+        end,
+      }
+    end,
   },
+
+  -- DAP
   {
-  "nvim-pack/nvim-spectre",
-  config = function()
-    vim.keymap.set("n", "<C-S-F>", function()
-      require("spectre").toggle()
-    end, { desc = "Toggle Spectre (Search & Replace)", noremap = true, silent = true })
-  end,
- },
- {
-  "lewis6991/gitsigns.nvim",
-  config = function()
-    require("gitsigns").setup {
-      on_attach = function(bufnr)
-        local gs = package.loaded.gitsigns
-
-        local function map(mode, l, r, opts)
-          opts = opts or {}
-          opts.buffer = bufnr
-          vim.keymap.set(mode, l, r, opts)
-        end
-
-        -- Navigation
-        map("n", "]c", function()
-          if vim.wo.diff then return "]c" end
-          vim.schedule(function() gs.next_hunk() end)
-          return "<Ignore>"
-        end, { expr = true, desc = "Next Hunk" })
-
-        map("n", "[c", function()
-          if vim.wo.diff then return "[c" end
-          vim.schedule(function() gs.prev_hunk() end)
-          return "<Ignore>"
-        end, { expr = true, desc = "Prev Hunk" })
-
-        -- Actions
-        map("n", "<leader>hs", gs.stage_hunk, { desc = "Stage Hunk" })
-        map("n", "<leader>hr", gs.reset_hunk, { desc = "Reset Hunk" })
-        map("v", "<leader>hs", function() gs.stage_hunk { vim.fn.line("."), vim.fn.line("v") } end, { desc = "Stage Hunk" })
-        map("v", "<leader>hr", function() gs.reset_hunk { vim.fn.line("."), vim.fn.line("v") } end, { desc = "Reset Hunk" })
-
-        map("n", "<leader>hp", gs.preview_hunk, { desc = "Preview Hunk" })
-        map("n", "<leader>hb", gs.blame_line, { desc = "Blame Line" })
-
-        map("n", "<leader>tb", gs.toggle_current_line_blame, { desc = "Toggle Line Blame" })
-      end,
-    }
-  end,
+    "mfussenegger/nvim-dap",
+    dependencies = {
+      {
+        "rcarriga/nvim-dap-ui",
+        dependencies = { "nvim-neotest/nvim-nio" },
+      },
+    },
+    config = function()
+      local dap = require("dap")
+      local dapui = require("dapui")
+      dapui.setup()
+      dap.listeners.after.event_initialized["dapui_config"] = function() dapui.open() end
+      dap.listeners.before.event_terminated["dapui_config"] = function() dapui.close() end
+      dap.listeners.before.event_exited["dapui_config"] = function() dapui.close() end
+    end,
   },
+
+  -- LSP
   {
-  "mfussenegger/nvim-dap",
-  dependencies = {
-    "rcarriga/nvim-dap-ui",
-	dependencies = {
-		"nvim-neotest/nvim-nio",
-	}
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+      -- Python
+      lspconfig.pyright.setup {}
+      -- C/C++
+      lspconfig.clangd.setup {}
+      -- Bash
+      lspconfig.bashls.setup {}
+      -- JSON
+      lspconfig.jsonls.setup {}
+      -- YAML
+      lspconfig.yamlls.setup {}
+      -- Lua
+      lspconfig.lua_ls.setup {
+        settings = {
+          Lua = {
+            diagnostics = { globals = { "vim" } },
+          },
+        },
+      }
+    end,
   },
-  config = function()
-    local dap = require("dap")
-    local dapui = require("dapui")
 
-    dapui.setup()
-    -- Automatically open dapui when starting debugger
-    dap.listeners.after.event_initialized["dapui_config"] = function()
-      dapui.open()
-    end
-    -- Automatically close dapui when ending debugger
-    dap.listeners.before.event_terminated["dapui_config"] = function()
-      dapui.close()
-    end
-    dap.listeners.before.event_exited["dapui_config"] = function()
-      dapui.close()
-    end
-  end,
-  }
+  -- Autocomplete
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline",
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
+    },
+    config = function()
+      local cmp = require("cmp")
+      cmp.setup {
+        snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<Tab>"] = cmp.mapping.select_next_item(),
+          ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+        }, {
+          { name = "buffer" },
+        }),
+      }
+    end,
+  },
+
+  -- Statusline
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("lualine").setup {
+        options = {
+          theme = "nord",
+          section_separators = "",
+          component_separators = "",
+        },
+      }
+    end,
+  },
 }
