0df8b6f8092b0ab5fa6141db86fd541ef5a67d5f
codebro <64486383+paulsso@users.noreply.github.com>
2025-06-25 08:40:56 +0200

    Update init.lua

--- scripts/settings/nvim/init.lua
+++ scripts/settings/nvim/init.lua
@@ -1,33 +1,65 @@
--- ~/.config/nvim/init.lua
-
--- Bootstrap lazy.nvim if not installed
+-- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
 vim.opt.rtp:prepend(lazypath)
 
+-- Load plugins
 require("lazy").setup("plugins")
 
+-- Set <leader> key to Space
+vim.g.mapleader = " "
+
 -- General settings
-vim.o.number = true
-vim.o.relativenumber = true
-vim.o.tabstop = 4
-vim.o.shiftwidth = 4
-vim.o.expandtab = false
-vim.o.mouse = "a"
-vim.o.termguicolors = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = false
+vim.opt.mouse = "a"
+vim.opt.termguicolors = true
+vim.opt.clipboard = "unnamedplus" -- enable system clipboard
 
--- Set Python host program
-vim.g.python3_host_prog = os.getenv("HOME") .. "/.venvs/default/bin/python"
+-- Python host program
+vim.g.python3_host_prog = vim.fn.expand("~/.venvs/default/bin/python")
 
-vim.keymap.set("n", "<C-S-F>", [[:%s///gc<Left><Left><Left>]], { noremap = true, silent = false })
+-- === Keymaps ===
 
-vim.opt.clipboard = "unnamedplus"
+-- Save with Ctrl+S
+vim.keymap.set("n", "<C-s>", ":w<CR>", { noremap = true, silent = true })
 
--- Load Telescope
-local telescope = require("telescope")
-local builtin = require("telescope.builtin")
-local live_grep_args = require("telescope").extensions.live_grep_args
+-- Move lines with Alt+J / Alt+K
+vim.keymap.set("n", "<A-j>", ":m .+1<CR>==", { noremap = true, silent = true })
+vim.keymap.set("n", "<A-k>", ":m .-2<CR>==", { noremap = true, silent = true })
+vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
+vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
 
--- CTRL+SHIFT+F → Live Grep with args (Search + Replace)
-vim.keymap.set("n", "<C-S-F>", function()
-  live_grep_args.live_grep_args()
-end, { noremap = true, silent = true })
+-- Tab to indent in Visual mode
+vim.keymap.set("v", "<Tab>", ">gv", { noremap = true, silent = true })
+vim.keymap.set("v", "<S-Tab>", "<gv", { noremap = true, silent = true })
+
+-- Telescope keymaps
+local ok, builtin = pcall(require, "telescope.builtin")
+if ok then
+  vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+  vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+end
+
+-- NvimTree keymap
+vim.keymap.set("n", "<C-n>", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
+
+-- Spectre keymap
+local ok_spectre, spectre = pcall(require, "spectre")
+if ok_spectre then
+  vim.keymap.set("n", "<C-S-F>", function()
+    spectre.toggle()
+  end, { desc = "Toggle Spectre (Search & Replace)", noremap = true, silent = true })
+end
